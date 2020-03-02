a7f55556bc8b473f0654a87036995c5ee90c6c6a
khoironi <masrony37@gmail.com>
2020-03-02 14:51:54 +0700

    new nav

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -26,7 +26,7 @@
 
 	<div class="container fixed-top">
 		<nav class="navbar navbar-expand-lg navbar-light ">
-			<a class="navbar-brand" href="<?= base_url() ?>"><?= $title; ?>.net</a>
+			<a class="navbar-brand" href="<?= base_url() ?>"><?= $title; ?></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
@@ -40,6 +40,9 @@
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="#">Contact</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Test</a>
 					</li>
 
 				</ul>
